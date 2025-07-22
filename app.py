bcf1ffa320d9b0a3c1ea4e4f8fbebefa0309b1ba
electrobiotica <157607088+electrobiotica@users.noreply.github.com>
2025-07-21 21:25:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,7 @@ def privacidad():
     return render_template("privacidad.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5123)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render asigna dinámicamente este puerto
+    app.run(host="0.0.0.0", port=port)
+
